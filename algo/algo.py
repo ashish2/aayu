d260e5672bcd229789dd6b5fb715a31bd141a854
Ashish <vickyojha2@yahoo.com>
2020-05-18 07:35:32 +0530

    U: Identification migration, and algo outline

---
+++ algo/algo.py
@@ -0,0 +1,25 @@
+
+# Check for 
+# How many People should be allowed,
+# In Which City
+# In Which Company
+# For How long
+
+def city():
+    """Area of city and How many people it can handle at a time"""
+    area = 1
+    return area
+
+def number_of_people_working_in_city():
+    number = 1
+    return number
+
+def city_can_handle():
+    handle = 1
+    return handle
+
+def check_for_previous_people_not_given_chance_allow_them():
+    not_given_chance_yet_allow = 1
+    return not_given_chance_yet_allow
+
+
