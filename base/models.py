72e998007e4001e014db6d4cd005bbed4a9b91b1
Ashish <vickyojha2@yahoo.com>
2020-05-21 06:37:10 +0530

    U: updating ...

--- base/models.py
+++ base/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class BaseModel(models.Model):
-    created_on = models.DateTimeField()
-    last_updated = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.SmallIntegerField(default=True) # status: values 0-Deactivated, 1-Active, 2-Pending, null-probably untouched yet
     class Meta:
         abstract = True
--- cities/models.py
+++ cities/models.py
@@ -2,12 +2,20 @@ from django.db import models
 
 # Create your models here.
 from base import BaseModel
+from company import Branch
+
+class State(BaseModel):
+    name = models.CharField()
 
 class City(BaseModel):
     name = models.CharField()
     latitude = models.DoubleField()
     longitude = models.DoubleField()
-    state = models.CharField()
+    state = models.ForeignKey(State)
     area_square_meters = models.PositiveIntegerField() # Area in Square-Meters
+
+
+#class CityBranch(BaseModel):
+    #pass
 
 
