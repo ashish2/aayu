8296acc298c27b1dc00847c888cae3c355f69c18
Ashish <vickyojha2@yahoo.com>
2020-05-21 07:18:18 +0530

    U: updates...

--- cities/models.py
+++ cities/models.py
@@ -11,7 +11,8 @@ class City(Base):
     latitude = models.DecimalField(decimal_places=7, max_digits=10)
     longitude = models.DecimalField(decimal_places=7, max_digits=10)
     state = models.ForeignKey(State, on_delete=models.CASCADE)
-    area_square_meters = models.PositiveIntegerField() # Area in Square-Meters
+    #area_square_meters = models.PositiveIntegerField() # Area in Square-KiloMeters, Mumbai=603.4 SqKilo
+    area = models.DecimalField(decimal_places=5, max_digits=15) # Area in Square-KiloMeters, Mumbai=603.4 SqKilo
 
 
 #class CityBranch(BaseModel):
