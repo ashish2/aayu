490ae267bcd8cf2655d10ca1f8b3ab1385bde64d
Ashish <vickyojha2@yahoo.com>
2020-05-23 00:06:03 +0530

    U: Readme...

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Aayu is an attempt to solve the question of How to save and secure lives of peop
 ## DB Struct ##
 Refer the DB Folder
 
+# TODO
+## Presentation
+Create 2 City
+Create 2 Company in Each City - Total 2 Companies Row: InfoSys, TCS
+Create 2 Branches of Both Companies with their SquareMeter Area in Both City - Total 4 Branches of 2 Companies - Total 8 Branch DBRows
+Create Some Number of Users for Each Branch
+And show all on Main Page of Greened Module 
+
+~ Main Page to Links of Multiple City 
+	Inside City Page Link
+		Company DropDown
+		Select Company to show their Data & Number of People Working there, But how Many it can Handle and therefore Allowed
+
+
--- userprofile/models.py
+++ userprofile/models.py
@@ -3,13 +3,16 @@ from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
 from base.models import BaseModel
-from city.models import City
+from cities.models import City
+from companies.models import Branch
 
 class UserProfile(BaseModel):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #location = models.CharField(max_length=30, blank=True)
+    phone = models.PositiveIntegerField(10)
+    branch = models.ForeignKey(Branch)
     city = models.ForeignKey(City)
     
+    #location = models.CharField(max_length=30, blank=True)
     #birthdate = models.DateField(null=True, blank=True)
     #role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, null=True, blank=True)
 
