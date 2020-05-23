4bf6aff020a32bc8ccfcdbfaa30be742c8f0d278
Ashish <vickyojha2@yahoo.com>
2020-05-23 13:19:15 +0530

    U: added UserProfile...

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,17 +1,25 @@
 from django.db import models
-
+from django.db.models import signals 
 # Create your models here.
 from django.contrib.auth.models import User
 from base.models import Base
 from cities.models import City
 from companies.models import Branch
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class UserProfile(Base):
+
+    class WorkType(models.TextChoices):
+        LA = ('LA', 'LABOUR'),
+        IT = ('IT', 'INFO TECH')
+
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    phone = models.PositiveIntegerField(10)
-    work_type = models.CharField(default=None)
-    branch = models.ForeignKey(Branch)
-    city = models.ForeignKey(City)
+    phone = models.PositiveIntegerField(10, default=None)
+    work_type = models.CharField( choices=WorkType.choices, default=WorkType.LA)
+    branch = models.ForeignKey(Branch, default=1)
+    city = models.ForeignKey(City, default=1)
     
     #location = models.CharField(max_length=30, blank=True)
     #birthdate = models.DateField(null=True, blank=True)
@@ -19,3 +27,17 @@ class UserProfile(Base):
 
     def __str__(self):  # __unicode__ for Python 2
         return self.user.username
+
+# SAVE SIGNAL
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+
+
