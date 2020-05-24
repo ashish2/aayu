671eb4455f61ffbccd8eb58b84c8e0a1c5496580
Ashish <vickyojha2@yahoo.com>
2020-05-25 03:47:16 +0530

    U: some need based logic...

--- greened/views.py
+++ greened/views.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 # Create your views here.
 from cities.models import City
 from companies.models import Branch
+from algo import algo
 
 class CitiesView(TemplateView):
     template_name = "city.html"
@@ -28,19 +29,22 @@ def details(request, city_id):
 
     return render(request, 'details.html', context)
 
-from algo import algo
 def branch_people(request, city_id, branch_id):
     city = City.objects.get(id=city_id)
     branch = Branch.objects.get(id=branch_id)
 
     min_dist = algo.minimum_distance_to_be_maintained()
-    handle = algo.how_many_can_be_handled(branch.area)
+    handle = round( algo.how_many_can_be_handled(branch.area) )
+
+    allowed_handle = round( handle - (handle * 20/100) )
 
     context = {
         'city': city,
         'branch': branch,
         'min_dist': min_dist,
-        'handle': handle
+        'handle': handle,
+        'allowed_handle': allowed_handle,
+        'not_allowed_handle': round( handle - allowed_handle ),
     }
 
     return render(request, 'branch_people.html', context)
